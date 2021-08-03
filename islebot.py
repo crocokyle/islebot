f400e3d632131e95cf8505b06d95ce75b76bc483
crocokyle <me@kyle-dixon.com>
2021-08-02 23:58:23 -1000

    Modified OCR to accept a list of search strings

--- islebot.py
+++ islebot.py
@@ -14,9 +14,11 @@ import numpy as np
 from PIL import ImageGrab
 import os
 import time
+from datetime import datetime
 import win32api, win32con
 import argparse
 import pynput
+import psutil
 from pynput.keyboard import Key, Controller
 
 
@@ -52,6 +54,13 @@ class mouse():
     def get_cords():
         x,y = win32api.GetCursorPos()
         print('Got mouse coordinates at: {}, {}'.format(x,y))
+
+
+def killTheIsle():
+    PROCNAME = "TheIsleClient-Win64-Shipping.exe"
+    for proc in psutil.process_iter():
+        if proc.name() == PROCNAME:
+            proc.kill()
 
 def getTesseractPath():
     if os.path.exists("tesseract_path.txt"):
@@ -108,22 +117,19 @@ def findButtonByColor(color):
             found_color = True
 
             return int(x), int(y)
-        
-        time.sleep(0.1)
 
 
 def findButtonByText(text):
-    found_text = False
-    print('Searching for "{}" button...'.format(text))
-    while not found_text:
-        
-        data = pytesseract.image_to_data(screenGrab(True), lang='eng', output_type=Output.DICT)
 
+    print('Searching for "{}" button...'.format(text))
+
+    while True:
+
+        data = pytesseract.image_to_data(screenGrab(True), lang='eng', output_type=Output.DICT)
         i = 0
+
         for string in data['text']:
             if text.lower() == string.lower():
-                found_text = True
-
                 left = int(data['left'][i])
                 top = int(data['top'][i])
                 right = int(data['left'][i]) + int(data['width'][i])
@@ -139,32 +145,100 @@ def findButtonByText(text):
                 return int(x), int(y)
 
             i+=1
-        time.sleep(0.05)
-    
 
-def findText(screenshot, text):
-    ocr = pytesseract.image_to_string(screenshot, lang='eng')
-    for string in text:
-        if string.lower() in ocr.lower():
-            print('Found text "{}"'.format(string))
-            return True
+
+def waitForText(text, timeout=0, debug=False):
+    print('Waiting for text: {} timeout in {}s...'.format(text, timeout))
+
+    if timeout:
+        start = time.time()
+
+    while True:
+        img = screenGrab(True)
+        data = pytesseract.image_to_data(img, lang='eng', output_type=Output.DICT)
+        if debug:
+            print(data)
+            img.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+
         
-    return False
+        for search_string in text:
+            i = 0
+            for ocr_string in data['text']:
+                if search_string.lower() in ocr_string.lower():
+                    left = int(data['left'][i])
+                    top = int(data['top'][i])
+                    right = int(data['left'][i]) + int(data['width'][i])
+                    bottom = int(data['top'][i]) + int(data['height'][i])
 
+                    print('Button bounding box: {}, {} x {}, {}'.format(top,left, bottom,right))
+                    
+                    x = ((right-left)/2)+left
+                    y = ((bottom-top)/2)+top
+
+                    print('Button center located at: {}, {}'.format(x, y))
+
+                    return int(x), int(y)
+
+                i+=1
+
+        if timeout and (time.time() - start) > timeout:
+            # Close the game and call main()
+            print("Sorry, we've run in to a problem. Islebot is starting over...")
+            killTheIsle()
+            main()
+            return False
 
 def launchGame():
     os.system('start steam://rungameid/376210')
 
 
-def waitForText(text):
-    found_text = False
-    print('Waiting for text: {}...'.format(text))
-    while not found_text:
-        
-        screenshot = screenGrab(False)
-        if findText(screenshot, text):
-            found_text = True
+def connectionLoop(server, timeout=0):
+
+    if timeout:
+        start = time.time()
+
+    while True: 
+        # Click "Play"
+        x, y = findButtonByText("Play")
+        mouse.setPos(x, y)
+        time.sleep(1)
+        mouse.leftClick(x,y)
+
+        # Find the Filter input box
+        print('Waiting for servers to load...')
+        x, y = findButtonByColor((155,179,174))
+        #x, y = findButtonByText("Filter")
+        mouse.setPos(x, y)
+        time.sleep(1)
+        mouse.leftClick(x,y)
+
+        # Enter the server name
+        time.sleep(0.5)
+        keyboard.type(server)
+
+        # Connect to the server
+        print("Analyzing search results...")
+        x, y = findButtonByColor((73,203,174)) # Click the server result
+        mouse.setPos(x, y)
+        time.sleep(1)
+        mouse.leftClick(x,y)
+        time.sleep(0.2)
+        mouse.leftClick(x,y)
+
+        # Did we connect successfully?
+        if waitForText(["Herbivore", "Carnivore", "Eggs", "Humans", "ASSET", "Logout", "Developer"], 60):
+            return True
+        else:
+            # If not, click Back
+            x, y = waitForText("Back", timeout=60)
+            mouse.setPos(x, y)
+            time.sleep(1)
+            mouse.leftClick(x,y)
+
         time.sleep(0.05)
+        if timeout and (time.time() - start) > timeout:
+            return False
+
 
 
 def main():
@@ -183,11 +257,9 @@ def main():
     # Set up notifications
     if args.sms:
         SMS = Notification("SMS", int(args.sms[0]))
-        #SMS.send()
 
     if args.email:
         EMAIL = Notification("Email", str(args.email[0]))   
-        #EMAIL.send()
 
     # Start the game and wait for the disclaimer
     print('Launching The Isle')
@@ -199,35 +271,12 @@ def main():
     mouse.setPos(x, y)
     time.sleep(1)
     mouse.leftClick(x,y)
-
-    # Click "Play"
-    x, y = findButtonByText("Play")
-    mouse.setPos(x, y)
-    time.sleep(1)
-    mouse.leftClick(x,y)
-
-    # Find the Filter input box
-    print('Waiting for servers to load...')
-    x, y = findButtonByColor((155,179,174))
-    mouse.setPos(x, y)
-    time.sleep(1)
-    mouse.leftClick(x,y)
-
-    # Enter the server name
-    time.sleep(0.5)
-    keyboard.type(str(args.SERVER[0]))
-
-    # Connect to the server
-    print("Analyzing search results...")
-    # Todo - Add timeout if no results are found (Means server is offline)
-    x, y = findButtonByColor((73,203,174))
-    mouse.setPos(x, y)
-    time.sleep(1)
-    mouse.leftClick(x,y)
-    time.sleep(0.2)
-    mouse.leftClick(x,y)
-
-    # Check if we're connected
+    if connectionLoop(str(args.SERVER[0])):
+        if args.sms:
+            SMS.send()
+        if args.email:
+            EMAIL.send()
+        print("Connected to the server! islebot is shutting down.")
 
 
 if __name__ == '__main__':
